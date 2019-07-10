3c1cd1100ca8e37c10e91d3a6a3cbae752279ea3
Stacey Smich <ssmich@Staceys-MacBook-Pro.local>
2019-07-10 11:47:42 -0600

    reviewed by Ryan

--- js/app.js
+++ js/app.js
@@ -5,27 +5,95 @@ function randomInt(max,min) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 }
 
-function dealCards() {
-  for(let i = 0; i <= 2; i++){
-    playerHand[i] = deck.splice(randomInt(deck.length,0),1);
-    computerHand[i] = deck.splice(randomInt(deck.length,0),1);
-    };
-  console.log(`Player and Computer have each been dealt 3 cards at random.`)
-  return console.log();
-};
-
-function playerChooses(){
-
-}
-
-class gameCards{
-  constructor(name, damagePts){
-    this.name = name;
-    this.damagePts = damagePts;
+function showPlayerHand(){
+  if(playerCards.length === 3){
+    console.log(`Player cards: 
+      ${playerCards[0].name} who causes ${playerCards[0].damagePts} in damage, 
+      ${playerCards[1].name} who causes ${playerCards[1].damagePts} in damage, 
+      and ${playerCards[2].name} who causes ${playerCards[2].damagePts} in damage`);
+  } else if(playerCards.length === 2){
+    console.log(`Player cards: 
+      ${playerCards[0].name} who causes ${playerCards[0].damagePts} in damage, 
+      and ${playerCards[1].name} who causes ${playerCards[1].damagePts} in damage`);
+  } else if (playerCards.length === 1){
+      console.log(`Player cards: 
+      ${playerCards[0].name} who causes ${playerCards[0].damagePts} in damage`);
+  } else {
+    console.log(`Player has no cards.`);
   }
 }
 
-let deck = new gameCards[
+function dealCards() {
+  for(let i = 0; i <= 2; i++){
+    playerCards[i] = deckCards.splice(randomInt(deckCards.length-1,0),1)[0];
+    computerCards[i] = deckCards.splice(randomInt(deckCards.length-1,0),1)[0];
+    };
+  console.log(`Player and Computer have each been dealt 3 cards at random.`)
+  console.log(`Player cards: ${playerCards[0].name} who causes ${playerCards[0].damagePts} in damage, 
+    ${playerCards[1].name} who causes ${playerCards[1].damagePts} in damage, 
+    and ${playerCards[2].name} who causes ${playerCards[2].damagePts} in damage`);
+};
+
+function battle(){
+  console.log(`Round ${round} of 3`);
+  let playerChoice = prompt("Type the name of the Pokemon chosen for battle.");
+  for(let i = 0; i < playerCards.length; i++){
+    if(playerChoice == playerCards[i].name){
+      battleCards[0] = playerCards.splice(i,1)[0];
+    };
+  }
+  battleCards[1] = computerCards.splice(randomInt(computerCards.length-1,0),1)[0];
+  console.log(`Computer chooses ${battleCards[1].name} for battle.`);
+  if(battleCards[0].damagePts > battleCards[1].damagePts){
+    playerScore++;
+    console.log(`Player wins round ${round}.`)
+  } else if(battleCards[0].damagePts < battleCards[1].damagePts){
+      computerScore++;
+      console.log(`Computer wins round ${round}.`);
+  } else {
+    console.log(`Battle tie, no points awarded.`);
+  };
+  disCards.push(battleCards[0], battleCards[1]);
+  round++;
+  scoreReport();
+  if(round <= 3){
+    showPlayerHand();
+    battle();
+  };
+};
+
+function cardCount(cardPile, name){
+  if(cardPile.length === 1){
+    console.log("The " + name + " has 1 card.");
+  } else {
+    console.log(`The ${name} has ${cardPile.length} cards.`);
+  };
+};
+
+
+function fullCardCount(){
+  cardCount(deckCards, "deck");
+  cardCount(playerCards, "player");
+  cardCount(computerCards, "computer");
+  if(battleCards.length === 2){
+    console.log(`The Player has the ${battleCards[0].name} card in play.`);
+    console.log(`The Computer has the ${battleCards[1].name} card in play.`);
+  } else if(battleCards.length !== 2){
+    console.log(`There are no cards in play.`);
+  };
+  cardCount(disCards, "already-played pile");
+};
+
+function scoreReport(){
+    console.log(`Player score: ${playerScore}, 
+    Computer score: ${computerScore}`);
+}
+
+function shuffleDeck(){
+
+}
+
+let deckCards = [
     {
       name: "Bulbasaur",
       damagePts: 60
@@ -81,21 +149,40 @@ let deck = new gameCards[
       name: "Weedle", 
       damagePts: 40
     }
-]
+];
+let playerCards = [];
+let computerCards = [];
+let disCards = [];
+let battleCards = [];
 
-let playerHand = new gameCards;
-let computerHand = new gameCards;
-let disCards = new gameCards;
-let battleCards = new gameCards;
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
+
 
 dealCards();
 
-console.log(deck.length, "deck");
-console.log(deck);
-console.log(playerHand.length, "player hand");
-console.log(playerHand);
-console.log(computerHand.length, "computerHand");
-console.log(computerHand);
+battle();
+
+
+// function cardCount(){
+//   console.log(`${deckCards.length}`, "deck",
+//   playerCards.length, "player",
+//   computerCards.length, "computer",
+//   disCards.length,"out-of-play",
+//   battleCards.length,"in-play");
+// dealCards();
+
+// console.log(deck.length, "deck",
+//   playerHand.length, "player",
+//   computerHand.length, "computer",
+//   disCards.length,"out-of-play",
+//   battleCards.length,"in-play");
+// console.log(deck);
+// console.log(playerHand.length, "player hand");
+// console.log(playerHand);
+// console.log(computerHand.length, "computerHand");
+// console.log(computerHand);
 
 // ## Example Play
 
